78ba3ceed0da39f23c5503b0fb7eb9e0d9cca1e1
Abhishek Rathore <abhishek@spacelinkers.com>
2025-08-21 15:05:35 +0530

    .

--- src/app/(User)/courses/page.jsx
+++ src/app/(User)/courses/page.jsx
@@ -17,7 +17,6 @@ import {
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import FAQ from "@/components/Animation/Faq";
 
 const LEVELS = [
   {
